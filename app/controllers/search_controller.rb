ebffaf464aded338aa4c8c139d0f3d8925e66c84
Kaito Yoshioka <kaitoyoshioka22@gmail.com>
2020-08-28 21:11:14 +0900

    有効なISBNの場合のみ書籍情報の取得と表示を行うようにした

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
-
 class SearchController < ApplicationController
   def index
     @book = Book.new(params[:isbn])
-    @book.fetch
+    @book.fetch if @book.valid_isbn?
   end
 end
