3251d5530d4ac05880659fce91a90665348b657a
Kaito Yoshioka <kaitoyoshioka22@gmail.com>
2020-08-29 01:49:17 +0900

    ISBNが入力されていない場合はreturnをするようにした

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 class SearchController < ApplicationController
   def index
+    return if params[:isbn].blank?
+
     @book = Book.new(params[:isbn])
     @book.fetch if @book.valid_isbn?
   end
