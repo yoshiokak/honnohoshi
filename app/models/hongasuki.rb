ae25364a344286abb152577d82c811230eb7fa14
Kaito Yoshioka <kaitoyoshioka22@gmail.com>
2020-09-07 19:54:04 +0900

    本が好き！から書籍の評価を取得できるようにした

---
+++ app/models/hongasuki.rb
@@ -0,0 +1,45 @@
+require 'open-uri'
+
+class Hongasuki
+  def initialize(isbn)
+    @isbn = isbn
+  end
+
+  def average_rating
+    "評価なし" if doc.at_css("b[itemprop='average']").text == "0"
+  end
+
+  def review_count
+    doc.at_css("b[itemprop='votes']").text
+  end
+
+  def name
+    "本が好き！"
+  end
+
+  def book
+    parse_book_path
+  end
+
+  private
+    def doc
+      Nokogiri::HTML.parse(URI.open(url))
+    end
+
+    def url
+      "https://www.honzuki.jp#{parse_book_path}"
+    end
+
+    def search_url
+      "https://www.honzuki.jp/book/book_search/index.html?search_in=honzuki&isbn=#{@isbn}"
+    end
+
+    def parse_book_path
+      doc = Nokogiri::HTML.parse(URI.open(search_url))
+      if doc.at_css("td > a").nil?
+        nil
+      else
+        doc.at_css("td > a").attributes["href"].value
+      end
+    end
+end
