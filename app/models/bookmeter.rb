aa2d98088508c888b7005b730e4347484aac11fb
Kaito Yoshioka <kaitoyoshioka22@gmail.com>
2020-10-05 20:45:21 +0900

    書籍の評価の取得を並列化した

--- app/models/bookmeter.rb
+++ app/models/bookmeter.rb
@@ -3,23 +3,20 @@
 require "open-uri"
 
 class Bookmeter
+  attr_reader :average_rating, :review_count, :url
+
   def initialize(isbn)
     @isbn = isbn
   end
 
-  def average_rating
-    if doc.at_css(".supplement__value.average").nil?
-      "評価なし"
-    else
-      (doc.at_css(".supplement__value.average").text.to_i * 0.05).round(2)
-    end
-  end
+  def fetch
+    @book = parse_book_path
 
-  def review_count
-    if doc.at_css(".supplement__value.count").nil?
-      "0"
-    else
-      doc.at_css(".supplement__value.count").text
+    if book_exists?
+      @url = parse_url
+      @doc = Nokogiri::HTML.parse(URI.open(@url))
+      @average_rating = parse_average_rating
+      @review_count = parse_review_count
     end
   end
 
@@ -27,19 +24,15 @@ class Bookmeter
     "読書メーター"
   end
 
-  def url
-    "https://bookmeter.com#{parse_book_path}"
-  end
-
-  def book
-    parse_book_path
+  def book_exists?
+    if @book.nil?
+      false
+    else
+      true
+    end
   end
 
   private
-    def doc
-      Nokogiri::HTML.parse(URI.open(url))
-    end
-
     def search_url
       "https://bookmeter.com/search?keyword=#{@isbn}"
     end
@@ -52,6 +45,26 @@ class Bookmeter
         nil
       else
         hash["resources"][0]["contents"]["book"]["path"]
+      end
+    end
+
+    def parse_url
+      "https://bookmeter.com#{parse_book_path}"
+    end
+
+    def parse_average_rating
+      if @doc.at_css(".supplement__value.average").nil?
+        "評価なし"
+      else
+        (@doc.at_css(".supplement__value.average").text.to_i * 0.05).round(2)
+      end
+    end
+
+    def parse_review_count
+      if @doc.at_css(".supplement__value.count").nil?
+        "0"
+      else
+        @doc.at_css(".supplement__value.count").text
       end
     end
 end
--- app/models/hongasuki.rb
+++ app/models/hongasuki.rb
@@ -3,39 +3,36 @@
 require "open-uri"
 
 class Hongasuki
+  attr_reader :average_rating, :review_count, :url
+
   def initialize(isbn)
     @isbn = isbn
   end
 
-  def average_rating
-    if doc.at_css("b[itemprop='average']").text == "0"
-      "評価なし"
-    else
-      doc.at_css("b[itemprop='average']").text
-    end
-  end
+  def fetch
+    @book = parse_book_path
 
-  def review_count
-    doc.at_css("b[itemprop='votes']").text
+    if book_exists?
+      @url = parse_url
+      @doc = Nokogiri::HTML.parse(URI.open(@url, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE))
+      @average_rating = parse_average_rating
+      @review_count = parse_review_count
+    end
   end
 
   def name
     "本が好き！"
   end
 
-  def url
-    "https://www.honzuki.jp#{parse_book_path}"
-  end
-
-  def book
-    parse_book_path
+  def book_exists?
+    if @book.nil?
+      false
+    else
+      true
+    end
   end
 
   private
-    def doc
-      Nokogiri::HTML.parse(URI.open(url, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE))
-    end
-
     def search_url
       "https://www.honzuki.jp/book/book_search/index.html?search_in=honzuki&isbn=#{@isbn}"
     end
@@ -47,5 +44,21 @@ class Hongasuki
       else
         doc.at_css("td > a").attributes["href"].value
       end
+    end
+
+    def parse_average_rating
+      if @doc.at_css("b[itemprop='average']").text == "0"
+        "評価なし"
+      else
+        @doc.at_css("b[itemprop='average']").text
+      end
+    end
+
+    def parse_review_count
+      @doc.at_css("b[itemprop='votes']").text
+    end
+
+    def parse_url
+      "https://www.honzuki.jp#{parse_book_path}"
     end
 end
