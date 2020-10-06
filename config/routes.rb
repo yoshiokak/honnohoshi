01a671f8df7397f4481b5d61a397175fb2dba2ef
Kaito Yoshioka <kaitoyoshioka22@gmail.com>
2020-10-07 07:35:16 +0900

    ヘルプページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'help/index'
   root to: "home#index"
   get "/search", to: "search#index"
   get :book_reviews, controller: :search
+  get "/help", to: "help#index"
 end
