4e62de3000d0c9e370639ba513a7cd3bb0aafd14
Kaito Yoshioka <kaitoyoshioka22@gmail.com>
2020-10-07 06:02:34 +0900

    rails g controller help index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'help/index'
   root to: "home#index"
   get "/search", to: "search#index"
   get :book_reviews, controller: :search
