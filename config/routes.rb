279a2cdbd67efefaf709bc15159e5df7dc2cf5b8
Kaito Yoshioka <kaitoyoshioka22@gmail.com>
2020-10-07 05:56:52 +0900

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get "home/index"
-  root to: "search#index"
+  root to: "home#index"
+  get "/search", to: "search#index"
   get :book_reviews, controller: :search
 end
