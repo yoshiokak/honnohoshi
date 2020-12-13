79240d48025992a5ddde89afc2f3547877545ddd
Kaito Yoshioka <kaitoyoshioka22@gmail.com>
2020-12-14 02:02:59 +0900

    テスト名を修正

--- test/models/rakuten_rapid_api/amazon_price_test.rb
+++ test/models/rakuten_rapid_api/amazon_price_test.rb
@@ -2,13 +2,15 @@
 
 require "test_helper"
 
-class AmazonTest < ActiveSupport::TestCase
-  setup do
-    stub_amazon
-  end
+module RakutenRapidAPI
+  class AmazonPriceTest < ActiveSupport::TestCase
+    setup do
+      stub_amazon
+    end
 
-  test ".search" do
-    assert_equal(JSON.parse(File.read(Rails.root.join("test/fixtures/files/amazon.json"))),
-                 RakutenRapidAPI::AmazonPrice.search("9784101010014"))
+    test ".search" do
+      assert_equal(JSON.parse(File.read(Rails.root.join("test/fixtures/files/amazon.json"))),
+                   RakutenRapidAPI::AmazonPrice.search("9784101010014"))
+    end
   end
 end
